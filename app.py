145fcb1d4a7c3d65ed40bf20270b0a391e4c73c7
NeryCaballero <nerycaballero24@gmail.com>
2021-03-29 15:39:25 +0200

    Added store-related endpoints

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -21,8 +23,10 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)
 
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 # avoids running app from an import
