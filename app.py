1dcd45b360a4287ce2866b441ba7c8fce42041f0
NeryCaballero <nerycaballero24@gmail.com>
2021-03-29 20:17:23 +0200

    Added PostgreSQL environment variable and psycopg2

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'nery'
 api = Api(app)
