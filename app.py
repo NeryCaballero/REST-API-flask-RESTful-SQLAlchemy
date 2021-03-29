ab9d7d101b9654f31a146b1bc960d34fcc49f3dc
NeryCaballero <nerycaballero24@gmail.com>
2021-03-29 20:08:38 +0200

    Added PostgreSQL environment variable and psycopg2

--- app.py
+++ app.py
@@ -11,16 +11,13 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'nery'
 api = Api(app)
-
-#
-# @app.before_first_request
-# def create_tables():
-#     db.create_all()
-#
 
 jwt = JWT(app, authenticate, identity)
 
@@ -28,11 +25,16 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
+
 api.add_resource(UserRegister, '/register')
 
-# avoids running app from an import
-# will run only if the file is being executed from running app.py
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
     app.run(port=5000, debug=True)
